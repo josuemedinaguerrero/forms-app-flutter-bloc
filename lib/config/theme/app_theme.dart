a19b35d8d1c3d20c0bfcbfe7b274ed58b0e30f69
Josue Medina Guerrero <josuemedina.032810@gmail.com>
2025-05-16 12:26:09 -0500

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() {
+    const seedColor = Colors.deepPurple;
+    return ThemeData(useMaterial3: true, colorSchemeSeed: seedColor, listTileTheme: ListTileThemeData(iconColor: seedColor));
+  }
+}
---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(create: (context) => CounterCubit(), child: _CubitCounterView());
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  void increateCounterBy(BuildContext context, [int value = 1]) {
+    context.read<CounterCubit>().increaseBy(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final counterState = context.watch<CounterCubit>().state;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cubit Counter: ${counterState.transactionCount}'),
+        actions: [IconButton(onPressed: () => context.read<CounterCubit>().reset(), icon: Icon(Icons.refresh_outlined))],
+      ),
+      body: Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          builder: (context, state) {
+            print('CONSTRUYENDO ANDO');
+            return Text('Counter value: ${state.counter}');
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(heroTag: '1', child: Text('+3'), onPressed: () => increateCounterBy(context, 3)),
+          SizedBox(height: 15),
+          FloatingActionButton(heroTag: '2', child: Text('+2'), onPressed: () => increateCounterBy(context, 2)),
+          SizedBox(height: 15),
+          FloatingActionButton(heroTag: '3', child: Text('+1'), onPressed: () => increateCounterBy(context, 1)),
+        ],
+      ),
+    );
+  }
+}
